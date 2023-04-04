7a9658d39fe42687387f77274ba5520ae179d3ce
Bebe-dev <praiseisinkaye@gmail.com>
2023-04-04 07:15:23 +0100

    updating the README file

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import search from './Images/search_icon.svg.png'
 import mic from './Images/Google_mic.svg.png'
 import lens from './Images/Google_lens.png'
 
 const SearchBar = () => {
+  const [word, setWord] = useState('hello');
   return(
-    <div className="input-div">
-      <img src={search} alt="search-icon" className="search-icon"/>
-      <input className="input"/>
-      <img src={mic} alt="google-mic" className="mic"/>
-      <img src={lens} alt="google-lens" className="lens"/>
-    </div>  
+    <>
+      <div className="input-div">
+        <img src={search} alt="search-icon" className="search-icon"/>
+        <input className="input"/>
+        <img src={mic} alt="google-mic" className="mic"/>
+        <img src={lens} alt="google-lens" className="lens"/>
+      </div>  
+      <div>
+        <p>{word}</p>
+      </div>
+    </>
   )
 }
 
