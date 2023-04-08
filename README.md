769293a520e4232107c2842274f77a78f2dc96af
Bebe-dev <praiseisinkaye@gmail.com>
2023-04-08 07:53:49 +0100

    Adding the search word functionality to it

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Component breakdown 
+This is a google homepage clone created using React and CSS. 
+It has a special functionality of being able to see the search words typed in the input right below it.
+
+# Google Component breakdown 
 ## Header section
 - Menu Item
 ## Search section
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import search from './Images/search_icon.svg.png'
 import mic from './Images/Google_mic.svg.png'
 import lens from './Images/Google_lens.png'
 
 const SearchBar = () => {
-  const inputValue = document.querySelector('.input');
   const [word, setWord] = useState('');
+  const [input, setInput] = useState('')
+  
+  const handleChange = (e) => {
+    const newWord = e.target.value
+    setInput(newWord)
+    
+  }
   const enterKey = (e) => {
-    if(e.key==="Enter"){;
-      setWord(word + e.target.value);
-      inputValue.value = ''
+    if(e.key==="Enter"){
+      if(input!==""){
+        setWord(input)
+        setInput("")
+      } else{
+        alert("You have to type an input")
+      }
+      
     }
   };
   return(
     <>
       <div className="input-div">
         <img src={search} alt="search-icon" className="search-icon"/>
-        <input onKeyPress={enterKey} className="input"/>
+        <input onKeyPress={enterKey} className="input" value={input} onChange={handleChange}/>
         <img src={mic} alt="google-mic" className="mic"/>
         <img src={lens} alt="google-lens" className="lens"/>
       </div>  
